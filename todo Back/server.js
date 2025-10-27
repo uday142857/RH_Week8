b8f6cce4f3721834047e080351e26e1fcae64c42
uday <dunna.udaykiran1729@gmail.com>
2025-10-27 10:41:12 +0530

    week8

---
+++ todo Back/server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ObjectId } = require("mongodb");
+const Mongodb_url = "mongodb://localhost:27017";
+const mongoclient = new MongoClient(Mongodb_url);
+
+const app = express();
+
+const PORT = "1313";
+
+app.use(cors());
+app.use(express.json());
+
+//http://localhost:1313/getdata
+app.get("/getdata", async (req, res) => {
+  await mongoclient.connect();
+  const db = mongoclient.db("rtodo_data");
+  const collection = db.collection("rdata");
+  const data = await collection.find().toArray();
+  // console.log(data);
+
+  res.json({
+    status: true,
+    data: data,
+  });
+});
+
+//http://localhost:1313/postdata
+app.post("/postdata", async (req, res) => {
+  const ldata = req.body;
+  await mongoclient.connect();
+  const db = mongoclient.db("rtodo_data");
+  const collection = db.collection("rdata");
+  const sdata = await collection.insertOne(ldata);
+
+  res.json({
+    status: true,
+    message: "Data Sended",
+  });
+});
+
+//http://localhost:1313/update
+app.put("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const {message} = req.body;
+   if (!message || message.trim() === "") {
+     return;
+   }
+  await mongoclient.connect();
+  const db = mongoclient.db("rtodo_data");
+  const collection = db.collection("rdata");
+  collection.updateOne({ _id: new ObjectId(id) }, { $set: { message: message } });
+
+  res.json({ status: true, message: "Todo updated successfully" });
+});
+//http://localhost:1313/remove
+app.delete("/remove/:id", async (req, res) => {
+  const id = req.params.id;
+  await mongoclient.connect();
+  const db = mongoclient.db("rtodo_data");
+  const collection = db.collection("rdata");
+  const data = await collection.deleteOne({ _id: new ObjectId(id) });
+
+  res.send({
+    status: true,
+    message: "Deleted ",
+  });
+});
+
+//http://localhost:1313
+app.listen(PORT, () => {
+  console.log("just Started");
+});
---
+++ todo/src/components/todo/ToDo.jsx
@@ -0,0 +1,151 @@
+import React, { useRef, useState } from "react";
+import "./ToDo.css";
+import { useEffect } from "react";
+
+function ToDo() {
+  const inputFocus = useRef(null);
+  const [add, setAdd] = useState("");
+  const [data, setData] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+  const [editId, setEditId] = useState(null);
+
+  useEffect(() => {
+    getdata();
+  }, []);
+  const getdata = () => {
+    fetch("http://localhost:1313/getdata")
+      .then((res) => {
+        return res.json();
+      })
+      .then((result) => {
+        console.log(result.data);
+        setData(result.data);
+      })
+      .catch((err) => [console.log("error", err)]);
+  };
+
+  const addData = () => {
+    if (add.trim() === "") return;
+    if (editId) {
+      // setData(
+      //   data.map((ele, index) => {
+      //     return index === editIndex ? add : ele;
+      //   })
+      // );
+
+       fetch(`http://localhost:1313/update/${editId}`, {
+         method: "PUT",
+         headers: { "Content-Type": "application/json" },
+         body: JSON.stringify({message : add}),
+       })
+         .then((res) => {
+           return res.json();
+         })
+         .then((result) => {
+           console.log("Data Updated");
+           setAdd("");
+           getdata();
+         })
+         .catch((err) => {
+           console.log("Erroe", err);
+         });
+      setEditIndex(null);
+      setEditId(null)
+      setAdd("");
+    } else {
+      fetch("http://localhost:1313/postdata", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({message:add})
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((result) => {
+          console.log("Data Sended");
+          setAdd("");
+          getdata()
+        })
+        .catch((err) => {
+          console.log("Erroe", err);
+        });
+      
+    }
+  };
+  const editItem = (index) => {
+    const item= data[index]
+    setEditIndex(index);
+    setEditId(item._id)
+    setAdd(item.message);
+    inputFocus.current.focus();
+  };
+
+  const removeData = (iD) => {
+    // setData((prev) => prev.filter((_, i) => i !== index));
+
+    fetch(`http://localhost:1313/remove/${iD}`, { method: "DELETE" })
+      .then((res) => {
+        return res.json();
+      })
+      .then((result) => {
+        console.log("deleted");
+        getdata();
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+  return (
+    <div className="main">
+      <div className="top">
+        <h1>TODO LIST</h1>
+        <input
+          type="text"
+          value={add}
+          ref={inputFocus}
+          onChange={(e) => {
+            setAdd(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              addData();
+            }
+          }}
+          placeholder="Add List"
+        />
+        <button onClick={addData}>Add</button>
+      </div>
+
+      <div className="list">
+        {data.map((ele, index) => {
+          return (
+            <div className="litem">
+              <div className="data">
+                <h4>{ele.message}</h4>
+              </div>
+              <div className="bt">
+                <input type="checkbox" />
+                <button
+                  onClick={() => {
+                    editItem(index);
+                  }}
+                >
+                  <i class="bi bi-pencil-square"></i>
+                </button>
+                <button
+                  onClick={() => {
+                    removeData(ele._id);
+                  }}
+                >
+                  <i class="bi bi-trash3-fill"></i>
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default ToDo;
